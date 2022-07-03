b5cd2404aec2cc05b92a96a1523b06948b9734e8
Amera Alleyne <amera.alleyne@gmail.com>
2022-07-03 18:21:27 -0400

    Added product/category count to admin dashboard

---
+++ app/assets/stylesheets/admin/dashboard_controller.rb
@@ -0,0 +1,15 @@
+class Admin::DashboardController < ApplicationController
+  def show
+    @categories = Category.count(:id)
+    @products = Product.all.count
+  end
+
+  def category_count
+    
+  end
+
+  def product_count
+    
+  end
+end
+Footer
