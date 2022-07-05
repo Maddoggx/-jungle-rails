0fc7ef1dd0206d2f0b1cb02b2adb8503db7cf4a0
Amera Alleyne <amera.alleyne@gmail.com>
2022-07-05 16:18:22 -0400

    user authentification

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ActiveRecord::Base
+#
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false },  format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 4 }
+
+
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where('lower(email) = ?', email.downcase.strip).first
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+  
+end
